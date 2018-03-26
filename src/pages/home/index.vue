7a0bf85df8d5b226c343b34e9f155e09f6694f80
SandyHellen <874322818@qq.com>
2018-03-26 10:59:45 +0800

    添加忽略文件

---
+++ src/pages/home/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h1>我是HomeIndex的标题</h1>
+  </div>
+</template>
+<script>
+export default {
+
+}
+</script>
+<style scoped>
+h1{
+   font-size: 40px;
+  color: red;
+}
+</style>
+
+
